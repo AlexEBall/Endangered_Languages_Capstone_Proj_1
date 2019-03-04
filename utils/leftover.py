5d9a030ab7c50d8cdf41ed8f2b07f74d8c0e8bdd
Alex Edward Ball <ballalexedward@gmail.com>
2019-03-03 16:21:27 -0800

    cleaned world bank gdp data set added, funciton to make readable numbers and one last piece to merge to final dataset

--- utils/leftover.py
+++ utils/leftover.py
@@ -8,3 +8,6 @@ plt.show()
 
 eng_speaking_countries = eng_speaking_countries.fillna(
     {'2018 Rank': 1.0, '2018 Score': 100.0, '2018 Band': 'Native Speakers'})
+
+gdp['GDP Average (Current US $)'] = gdp['GDP Average (Current US $)'].fillna(0.0).astype(int)
+gdp['GDP Average (Current US $)'] = [ millify(x) for x in gdp['GDP Average (Current US $)']]
